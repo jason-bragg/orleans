c16caeca245c67c2d1e5b7a3fc01b8dfb2cfd6b8
jason-bragg <jbragg@microsoft.com>
2019-03-04 17:57:03 -0800

    Proposed public surface changes to support batching.

--- src/Orleans.Core.Abstractions/Streams/Core/IAsyncObservable.cs
+++ src/Orleans.Core.Abstractions/Streams/Core/IAsyncObservable.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Orleans.Streams
@@ -44,5 +45,37 @@ namespace Orleans.Streams
         Task<StreamSubscriptionHandle<T>> SubscribeAsync(IAsyncObserver<T> observer, StreamSequenceToken token,
             StreamFilterPredicate filterFunc = null,
             object filterData = null);
+    }
+
+    /// <summary>
+    /// This interface generalizes the IAsyncObserver interface to allow production and consumption of batches of items.
+    /// <para>
+    /// Note that this interface is implemented by item consumers and invoked (used) by item producers.
+    /// This means that the consumer endpoint of a stream implements this interface.
+    /// </para>
+    /// </summary>
+    /// <typeparam name="T">The type of object consumed by the observer.</typeparam>
+    public interface IAsyncBatchObservable<T>
+    {
+        /// <summary>
+        /// Subscribe a consumer to this batch observable.
+        /// </summary>
+        /// <param name="observer">The asynchronous batch observer to subscribe.</param>
+        /// <returns>A promise for a StreamSubscriptionHandle that represents the subscription.
+        /// The consumer may unsubscribe by using this handle.
+        /// The subscription remains active for as long as it is not explicitly unsubscribed.
+        /// </returns>
+        Task<StreamSubscriptionHandle<T>> SubscribeAsync(IAsyncBatchObserver<T> observer);
+
+        /// <summary>
+        /// Subscribe a consumer to this batch observable.
+        /// </summary>
+        /// <param name="observer">The asynchronous batch observer to subscribe.</param>
+        /// <param name="token">The stream sequence to be used as an offset to start the subscription from.</param>
+        /// <returns>A promise for a StreamSubscriptionHandle that represents the subscription.
+        /// The consumer may unsubscribe by using this handle.
+        /// The subscription remains active for as long as it is not explicitly unsubscribed.
+        /// </returns>
+        Task<StreamSubscriptionHandle<T>> SubscribeAsync(IAsyncBatchObserver<T> observer, StreamSequenceToken token);
     }
 }
--- src/Orleans.Core.Abstractions/Streams/Core/IAsyncObserver.cs
+++ src/Orleans.Core.Abstractions/Streams/Core/IAsyncObserver.cs
@@ -58,6 +58,18 @@ namespace Orleans.Streams
         Task OnErrorAsync(Exception ex);
     }
 
+    public class OrderedStreamEvent<T>
+    {
+        public OrderedStreamEvent(T item, StreamSequenceToken token)
+        {
+            this.Item = item;
+            this.Token = token;
+        }
+
+        public T Item { get; }
+        public StreamSequenceToken Token { get; }
+    }
+
     /// <summary>
     /// This interface generalizes the IAsyncObserver interface to allow production and consumption of batches of items.
     /// <para>
@@ -66,22 +78,48 @@ namespace Orleans.Streams
     /// </para>
     /// </summary>
     /// <typeparam name="T">The type of object consumed by the observer.</typeparam>
-    public interface IAsyncBatchObserver<in T> : IAsyncObserver<T>
+    public interface IAsyncBatchObserver<T>
     {
         /// <summary>
         /// Passes the next batch of items to the consumer.
         /// <para>
-        /// The Task returned from this method should be completed when all items in the batch have been
+        /// The Task returned from this method should be completed when the items' processing has been
         /// sufficiently processed by the consumer to meet any behavioral guarantees.
         /// </para>
         /// <para>
-        /// That is, the semantics of the returned Task is the same as for <code>OnNextAsync</code>,
-        /// extended for all items in the batch.
+        /// When the consumer is the (producer endpoint of) a stream, the Task is completed when the stream implementation
+        /// has accepted responsibility for the items and is assured of meeting its delivery guarantees.
+        /// For instance, a stream based on a durable queue would complete the Task when the items have been durably saved.
+        /// A stream that provides best-effort at most once delivery would return a Task that is already complete.
+        /// </para>
+        /// <para>
+        /// When the producer is the (consumer endpoint of) a stream, the Task should be completed by the consumer code
+        /// when it has accepted responsibility for the items. 
+        /// In particular, if the stream provider guarantees at-least-once delivery, then the items should not be considered
+        /// delivered until the Task returned by the consumer has been completed.
         /// </para>
         /// </summary>
-        /// <param name="batch">The items to be passed.</param>
-        /// <param name="token">The stream sequence token of this batch of items.</param>
-        /// <returns>A Task that is completed when the batch has been accepted.</returns>
-        Task OnNextBatchAsync(IEnumerable<T> batch, StreamSequenceToken token = null);
+        /// <param name="items">The item to be passed.</param>
+        /// <returns>A Task that is completed when the item has been accepted.</returns>
+        Task OnNextAsync(IList<OrderedStreamEvent<T>> items);
+
+        /// <summary>
+        /// Notifies the consumer that the stream was completed.
+        /// <para>
+        /// The Task returned from this method should be completed when the consumer is done processing the stream closure.
+        /// </para>
+        /// </summary>
+        /// <returns>A Task that is completed when the stream-complete operation has been accepted.</returns>
+        Task OnCompletedAsync();
+
+        /// <summary>
+        /// Notifies the consumer that the stream had an error.
+        /// <para>
+        /// The Task returned from this method should be completed when the consumer is done processing the stream closure.
+        /// </para>
+        /// </summary>
+        /// <param name="ex">An Exception that describes the error that occurred on the stream.</param>
+        /// <returns>A Task that is completed when the close has been accepted.</returns>
+        Task OnErrorAsync(Exception ex);
     }
 }
